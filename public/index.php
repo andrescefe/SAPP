d190f33da0caa6916edded113b46fc4eea62ca3b
andrescefe <andrescefe@gmail.com>
2016-09-18 12:24:16 -0500

    se crea esqueleto de la aplicacion

--- public/index.php
+++ public/index.php
@@ -12,8 +12,19 @@ define("PROJECTPATH", dirname(__DIR__));
 //directorio app
 define("APPPATH", PROJECTPATH . '/App');
 
-
 require __DIR__ . '/../vendor/autoload.php';
+
+//autoload con namespaces
+function autoload_classes($class_name)
+{
+    $filename = PROJECTPATH . '/' . str_replace('\\', '/', $class_name) .'.php';
+    if(is_file($filename))
+    {
+        include_once $filename;
+    }
+}
+//registramos el autoload autoload_classes
+spl_autoload_register('autoload_classes');
 
 //instancia de la app
 $app = new \Core\App;
